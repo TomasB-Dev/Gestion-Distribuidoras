aae3d2d3c59d4cec162f51aaa13da624ae6f37f1
GordoPipoca <152812185+GordoPipoca@users.noreply.github.com>
2024-08-26 20:03:09 -0300

    add function to open excels files

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import tkinter as tk
-import openpyxl
+from tkinter import messagebox, filedialog
+from openpyxl import load_workbook, Workbook
 #COLORES
 GRIS = "#d8cccc"
 NEGRO = "#111111"
+def Cargar_datos_Excel(archivo):
+    wb = load_workbook(archivo)
+    
+def Cargar_Datos():#seleccionar el archivo a analizar
+    archivo = filedialog.askopenfilename(title="Seleccionar archivo de Excel", filetypes=(("Archivos de Excel", "*.xlsx"), ("Todos los archivos", "*.*")))
+    if archivo:
+        global registros
+        registros = Cargar_datos_Excel(archivo)
+        messagebox.showinfo("Éxito", "Los datos fueron cargados")
 
 def Main_Menu():#funciones del menu principal
     for widget in root.winfo_children(): #limpiar la screen cada vez que llama la funcion
         widget.destroy()
     #Botones del meno principal
-    btn_Cargar = tk.Button(root, text="Cargar Datos", bg=GRIS,fg=NEGRO,width=20,height=3)
+    btn_Cargar = tk.Button(root, text="Cargar Datos", bg=GRIS,fg=NEGRO,width=20,height=3, command=Cargar_Datos)
     btn_Cargar.pack(side=tk.LEFT, padx=10,pady=10)
 
     btn__Ver_Registros = tk.Button(root,text="Ver Registros", bg=GRIS,fg=NEGRO,width=20,height=3)
@@ -20,6 +30,6 @@ root = tk.Tk()
 root.title("Distribuidoras Mabel")
 root.geometry("720x460")
 root.config(bg="GRAY")
-
+registros = []#para almacenar los registros
 Main_Menu()
 root.mainloop()
