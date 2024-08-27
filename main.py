c0fc3a21c8a0acf3529aa7d272659b41abc72556
GordoPipoca <152812185+GordoPipoca@users.noreply.github.com>
2024-08-27 11:44:08 -0300

    fix bugs

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ def Cargar_datos_Excel(archivo):# carga datos del excel
 def Guarda_Datos_En_Excel(records, cliente_promedio):
     wb = Workbook()
     ws = wb.active
-    fecha = datetime.now()
-    ws.title = f"Promedios{fecha}"
+    ws.title = f"Promedios"
     ws["A1"] = "Distribuidora"
     ws["B1"] = "Fecha"
     ws["C1"] = "Distco"
@@ -33,13 +32,13 @@ def Guarda_Datos_En_Excel(records, cliente_promedio):
     row = 2 #contador
 
     for record in records:
-        ws[f"A{row}"] = record[0]
-        ws[f"B{row}"] = record[4]
-        ws[f"C{row}"] = record[2]
-        ws[f"D{row}"] = cliente_promedio[record[0]]
+        ws[f"A{row}"] = record[0].value
+        ws[f"B{row}"] = record[4].value
+        ws[f"C{row}"] = record[2].value
+        ws[f"D{row}"] = cliente_promedio[record[0].value]
         row += 1
     
-    wb.save(f"Promedios de {fecha}")
+    wb.save(f"Promedios.xlsx")
     messagebox.showinfo("EXITO","El archivo fue creado con exito.")
 
 
@@ -54,16 +53,16 @@ def Cargar_Datos():#seleccionar el archivo a cargar
 def Calcular_Promedio_Individual(records):
     client_volumen = {}
     for record in records:
-        client = record[0]
+        client = record[0].value
         if client not in client_volumen:
             client_volumen[client] = []
-        client_volumen[client].append(record[3])
+        client_volumen[client].append(record[3].value)
         
     cliente_promedio = {client: sum(volumen) / len(volumen) for client, volumen in client_volumen.items()}
     return cliente_promedio
 
 def Calcular_Volumen_Promedio(records):#calcula el promedio total
-    volumen_Total = sum(record[3] for record in records)
+    volumen_Total = sum(record[3].value for record in records)
     Volumen_Promedio = volumen_Total / len(records)
     return Volumen_Promedio
 
@@ -80,12 +79,28 @@ def Calcular_Promedios():
     def Enviar_Promedio():
         fecha_inicio = entry_Fecha_Inicio.get_date()
         print(f"{fecha_inicio}")# mausqueherramienta
-        parametros = [record for record in registros if len(record) == 5 and isinstance(record[4], datetime) and record[4].date() >= fecha_inicio]
+        parametros = []
+        for record in registros:
+            if len(record) == 5:
+                fecha_record = record[4].value
+                if isinstance(fecha_record, str):
+                    try:
+                        fecha_record = datetime.strptime(fecha_record, "%Y/%m/%d").date()
+                    except ValueError:
+                        continue
+                
+                if isinstance(fecha_record, datetime):
+                    fecha_record = fecha_record.date()
+                    
+                if fecha_record >= fecha_inicio:
+                    parametros.append(record)
+        
         if not parametros:
             messagebox.showerror("Sin datos", "No hay registros desde la fecha seleccionada.")
             return
         volumen_Promedio = Calcular_Volumen_Promedio(parametros)
         cliente_promedio = Calcular_Promedio_Individual(parametros)
+        Guarda_Datos_En_Excel(parametros,cliente_promedio)
         messagebox.showinfo("Promedio de Volumen", f"El promedio de volumen desde {fecha_inicio} es {volumen_Promedio}")
 
     btn_Calcular_Promedio = tk.Button(root,text="Calcular", bg=GRIS,fg=NEGRO, command=Enviar_Promedio) #btn para calcular el promedio
